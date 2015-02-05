a225fe2a644f2e1f551db14fd880083ca4aed69f
QamarWaqar <qamogle@gmail.com>
2015-02-05 22:15:02 +0500

    Twitter Tweet Stream Feed
    Eh nothing much but implemented twitter tweet stream.

--- TweetTest/Program.cs
+++ TweetTest/Program.cs
@@ -3,19 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace TweetTest
 {
     class Program
     {
-        private const string USER_SCREEN_NAME_TO_TEST = "Cortama";
+        //private const string USER_SCREEN_NAME_TO_TEST = "Cortama";
         static void Main(string[] args)
         {
-            //gettingToken("XsUQvfcCvHdE1MrbPDwJDuSlK", "G20QAv7p1LgCYYW4dqe62xqqOYI6M8bT1nnNiF0QUxDyNbldQp");
             //Tweetinvi.TwitterCredentials.SetCredentials("Access_Token", "Access_Token_Secret", "Consumer_Key", "Consumer_Secret");
 
             //gettingToken("XsUQvfcCvHdE1MrbPDwJDuSlK", "G20QAv7p1LgCYYW4dqe62xqqOYI6M8bT1nnNiF0QUxDyNbldQp");
+
             Tweetinvi.TwitterCredentials.SetCredentials("2462582037-Jf1etrbYD1MVpovzr7rtGnIn7fvQfqXz875DPpF", "ge391MZ3A6RAFkU5PFguS1O0seY0nxgxmQZZXpZ17a4mw", "XsUQvfcCvHdE1MrbPDwJDuSlK", "G20QAv7p1LgCYYW4dqe62xqqOYI6M8bT1nnNiF0QUxDyNbldQp");
+
             // This is your tweet, save, its ID to delete it in future //
             //Tweetinvi.Core.Interfaces.ITweet iT = Tweetinvi.User.GetLoggedUser().PublishTweet("Hello World!");
             //Console.WriteLine("Tweet ID: " + iT.Id);
@@ -26,8 +28,18 @@ namespace TweetTest
             //Console.WriteLine(Tweetinvi.User.GetLoggedUser().CreatedAt.ToString());
             //Console.WriteLine(Tweetinvi.User.GetLoggedUser().Notifications.ToString());
             //Console.WriteLine(Tweetinvi.User.GetLoggedUser().Name);
+
+            Tweetinvi.Core.Interfaces.Streaminvi.IUserStream iUS = Tweetinvi.Stream.CreateUserStream();
+            iUS.TweetCreatedByAnyone += IUS_TweetCreatedByAnyone;
+            iUS.StartStream();
         }
 
+        private static void IUS_TweetCreatedByAnyone(object sender, Tweetinvi.Core.Events.EventArguments.TweetReceivedEventArgs e)
+        {
+            Console.Beep();
+            Console.WriteLine(e.Tweet.Text);
+            throw new NotImplementedException();
+        }
         public static void gettingToken(string consumerKey, string consumerSecret)
         {
             var appCredentials = Tweetinvi.CredentialsCreator.GenerateApplicationCredentials(consumerKey, consumerSecret);
@@ -41,7 +53,8 @@ namespace TweetTest
             var newCredentials = Tweetinvi.CredentialsCreator.GetCredentialsFromVerifierCode(captcha, appCredentials);
             Console.WriteLine("Access Token = {0}", newCredentials.AccessToken);
             Console.WriteLine("Access Token Secret = {0}", newCredentials.AccessTokenSecret);
-            System.Diagnostics.Debug.WriteLine("Access Token = " + newCredentials.AccessToken); System.Diagnostics.Debug.WriteLine("Access Token Secret = " + newCredentials.AccessTokenSecret);
+            System.Diagnostics.Debug.WriteLine("Access Token = " + newCredentials.AccessToken);
+            System.Diagnostics.Debug.WriteLine("Access Token Secret = " + newCredentials.AccessTokenSecret);
         }
     }
 }
