0cf42e1ed15b897f819d7c8cda0afb0214fce3f0
QamarWaqar <qamogle@gmail.com>
2015-02-06 22:30:59 +0500

    Token Rate Limit Implemented.
    Showing Token Rate Limit at the start of App.

--- TweetTest/Program.cs
+++ TweetTest/Program.cs
@@ -29,16 +29,27 @@ namespace TweetTest
             //Console.WriteLine(Tweetinvi.User.GetLoggedUser().Notifications.ToString());
             //Console.WriteLine(Tweetinvi.User.GetLoggedUser().Name);
 
+            gettingTokenRateLimit("2462582037-Jf1etrbYD1MVpovzr7rtGnIn7fvQfqXz875DPpF", "ge391MZ3A6RAFkU5PFguS1O0seY0nxgxmQZZXpZ17a4mw", "XsUQvfcCvHdE1MrbPDwJDuSlK", "G20QAv7p1LgCYYW4dqe62xqqOYI6M8bT1nnNiF0QUxDyNbldQp");
+
             Tweetinvi.Core.Interfaces.Streaminvi.IUserStream iUS = Tweetinvi.Stream.CreateUserStream();
             iUS.TweetCreatedByAnyone += IUS_TweetCreatedByAnyone;
             iUS.StartStream();
+
+            //var tokenRateLimits = Tweetinvi.RateLimit.GetCurrentCredentialsRateLimits();
+            //Console.WriteLine("Limit: " + tokenRateLimits.ApplicationRateLimitStatusLimit.Limit);
+            //Console.WriteLine("Remaining: " + tokenRateLimits.ApplicationRateLimitStatusLimit.Remaining);
+            //Console.WriteLine("Reset Date and Time: " + tokenRateLimits.ApplicationRateLimitStatusLimit.ResetDateTime.ToString());
+            //Console.WriteLine("-----");
+
+
         }
 
         private static void IUS_TweetCreatedByAnyone(object sender, Tweetinvi.Core.Events.EventArguments.TweetReceivedEventArgs e)
         {
             Console.Beep();
             Console.WriteLine(e.Tweet.Text);
-            throw new NotImplementedException();
+            Console.WriteLine("----------");
+            //throw new NotImplementedException();
         }
         public static void gettingToken(string consumerKey, string consumerSecret)
         {
@@ -55,6 +66,28 @@ namespace TweetTest
             Console.WriteLine("Access Token Secret = {0}", newCredentials.AccessTokenSecret);
             System.Diagnostics.Debug.WriteLine("Access Token = " + newCredentials.AccessToken);
             System.Diagnostics.Debug.WriteLine("Access Token Secret = " + newCredentials.AccessTokenSecret);
+        }
+        /*public static Task func()
+        {
+            Task t = new Task(() =>
+            {
+                gettingTokenRateLimit("2462582037-Jf1etrbYD1MVpovzr7rtGnIn7fvQfqXz875DPpF", "ge391MZ3A6RAFkU5PFguS1O0seY0nxgxmQZZXpZ17a4mw", "XsUQvfcCvHdE1MrbPDwJDuSlK", "G20QAv7p1LgCYYW4dqe62xqqOYI6M8bT1nnNiF0QUxDyNbldQp");
+            });
+            t.Start();
+            return t;
+        }
+        public static async void fun()
+        {
+            await func();
+        }*/
+        public static void gettingTokenRateLimit(string accessToken, string accessTokenSecret, string consumerKey, string consumerSecret)
+        {
+            var credentials = Tweetinvi.TwitterCredentials.CreateCredentials(accessToken, accessTokenSecret, consumerKey, consumerSecret);
+            var tokenRateLimits = Tweetinvi.RateLimit.GetCredentialsRateLimits(credentials);
+
+            Console.WriteLine("Remaning Requests for GetRate : {0}", tokenRateLimits.ApplicationRateLimitStatusLimit.Remaining);
+            Console.WriteLine("Total Requests Allowed for GetRate : {0}", tokenRateLimits.ApplicationRateLimitStatusLimit.Limit);
+            Console.WriteLine("GetRate limits will reset at : {0} local time", tokenRateLimits.ApplicationRateLimitStatusLimit.ResetDateTime.ToLongTimeString());
         }
     }
 }
